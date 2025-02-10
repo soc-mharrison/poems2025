440056330d156c55eb0e9c30d24c02f75cc49f38
soc-mharrison <137470944+soc-mharrison@users.noreply.github.com>
2025-02-11 11:33:28 +1300

    Update task1-v1.py
    updated so it asks username
    says hello
    asks user age
    calculates age +1
    says you will be age+1 next year.

--- task1-v1.py
+++ task1-v1.py
@@ -1,19 +1,25 @@
 '''
 Task 1
 by Matt Harrison
-Date: Tues 04.02.2025
-Version 1
+Date: Tues 11.02.2025
+Version 3
 '''
 
-# this app will: 
-
 # display "Kia ora!"
+print("Kia ora!")
+
 # ask "What's your name?"   
 # (Tip user inputs name - e.g. 'Sam')
+name = input("What's your name?")
 
 # display user's name, e.g. "Hello, Sam"
+print("Hello, ", name)
 
 # ask the user for their age, e.g. "How old are you Sam?"
 # (Tip store the number as a variable, e.g. age)
+age = int(input("How old are you", name, "?"))
 
 # display "Next year you will be X years old"
+next_year = age + 1
+
+print("Next year you will be ", next_year , " years old")

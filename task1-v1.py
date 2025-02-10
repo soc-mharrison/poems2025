d48ca3b83949f5849c7b5f363ca72d9bb33eed1d
soc-mharrison <137470944+soc-mharrison@users.noreply.github.com>
2025-02-11 10:54:47 +1300

    Create task1-v1.py
    Basic .py file with comments
    '''multi-line header 
    Title, name, date, version
    '''
    
    # display "Kia ora!"
    # ask "What's your name?"   
    # (Tip user inputs name - e.g. 'Sam')
    # display user's name, e.g. "Hello, Sam"
    # ask the user for their age, e.g. "How old are you Sam?"
    # (Tip store the number as a variable, e.g. age)
    # display "Next year you will be X years old"

---
+++ task1-v1.py
@@ -0,0 +1,19 @@
+'''
+Task 1
+by Matt Harrison
+Date: Tues 04.02.2025
+Version 1
+'''
+
+# this app will: 
+
+# display "Kia ora!"
+# ask "What's your name?"   
+# (Tip user inputs name - e.g. 'Sam')
+
+# display user's name, e.g. "Hello, Sam"
+
+# ask the user for their age, e.g. "How old are you Sam?"
+# (Tip store the number as a variable, e.g. age)
+
+# display "Next year you will be X years old"
